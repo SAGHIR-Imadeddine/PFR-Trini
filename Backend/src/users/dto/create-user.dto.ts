f492c63c296a4229bec74fd4938b0506c4beab0b
SAGHIR-Imadeddine <sag.imadeddine@gmail.com>
2025-04-27 16:53:03 +0100

    TN-1 Feat: User CRUD

--- Backend/src/users/dto/create-user.dto.ts
+++ Backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,40 @@
-export class CreateUserDto {}
+import { 
+    IsString, 
+    IsNotEmpty, 
+    MinLength, 
+    MaxLength, 
+    IsEmail, 
+    IsEnum,
+    IsOptional,
+    IsArray
+} from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+import { Types } from 'mongoose';
+
+export class CreateUserDto {
+    
+    @IsNotEmpty({ message: 'Name is required' })
+    @IsString({ message: 'Name must be a string' })
+    @MinLength(3, { message: 'A valid name must be between 3 and 30 characters' })
+    @MaxLength(30, { message: 'A valid name must be between 3 and 30 characters' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Email is required' })
+    @IsEmail({}, { message: 'Enter a valid email' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
+    @IsString({ message: 'Password must be a string' })
+    @MinLength(8, { message: 'A valid password must be between 8 and 26 characters' })
+    @MaxLength(26, { message: 'A valid password must be between 8 and 26 characters' })
+    password: string;
+
+    @IsNotEmpty({ message: 'Role is required' })
+    @IsEnum(UserRole, { message: 'Role must be SUPER_ADMIN, OWNER, or EMPLOYEE' })
+    role: UserRole;
+
+    @IsOptional()
+    @IsArray({ message: 'Gyms must be an array of Gym IDs' })
+    gyms?: Types.ObjectId[];
+}
+
--- Backend/src/users/users.service.ts
+++ Backend/src/users/users.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectModel(User.name) private userModel : Model<UserDocument>
+  ){}
+
+  async create(createUserDto: CreateUserDto) : Promise<UserDocument | null> {
+    
+    const salt = await bcrypt.genSalt(10);
+    createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
+
+    const newUser = new this.userModel(createUserDto);
+    
+     return await newUser.save();
+    
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().select('-password').exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email }).exec();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: string): Promise<UserDocument | null> {
+    return this.userModel.findById(id).select('-password').exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, {new: true}).select('-password').exec();
+  }
+
+  async remove(id: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(id).exec();
   }
 }
